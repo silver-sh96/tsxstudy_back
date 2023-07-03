897f5bf46f6a0aae041af77c8fc0b88b697f8f86
silver-sh96 <cubha@naver.com>
2023-07-03 12:01:38 +0900

    중간저장_0703

--- src/main/java/com/study/tsxstudy_back/controller/BoardController.java
+++ src/main/java/com/study/tsxstudy_back/controller/BoardController.java
@@ -1,14 +1,23 @@
 package com.study.tsxstudy_back.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.study.tsxstudy_back.dto.Board;
 import com.study.tsxstudy_back.service.BoardService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -19,11 +28,20 @@ public class BoardController {
     private BoardService boardService;
 
     // 게시글 목록
-    @GetMapping("/boardList")
+    @RequestMapping("/boardList")
     public List<Board> getBoardList(){
         List<Board> boardList = boardService.getBoardList();
         log.info("boardList : {} ", boardList);
         return boardList;
+    }
+
+    // 게시글 작성
+    @RequestMapping("/boardWrite")
+    public int boardWrite(MultipartFile[] multipartFiles, Board board){
+        int result = 1;
+        String UPLOAD_PATH = "D:\\uploads"; // 업로드 할 위치
+
+        return result;
     }
     
 }
